ba283fa93faff77ac910c5847772a6cb640d3987
K-Yevhenii <ya.korniakov@gmail.com>
2024-09-02 20:40:27 +0300

    Home task

---
+++ tasks/For cycle.js
@@ -0,0 +1,29 @@
+// ## `For` cycle
+
+// ### Count amount of letters
+
+//     ** Goal ** - write a function that:
+
+//     1. Asks user a letter(skip all validation, it's assumed that a user types a single letter)
+// 2. Asks user a word
+// 3. Calculates how many times a letter is found in the word and returns this count.
+
+// For example, if user typed letter `a` and entered`banana`, the function should return `3`
+// Write 2 versions using `for` cycle and`while`
+
+//     ```js
+// // Version with `for`
+// function countLettersInWord() {}
+// ```
+
+//     ```js
+// // Version with `while`
+// function countLettersInWord() {}
+
+function countLettersInWord() {
+    let answer;
+    while (answer !== '') {
+        answer = prompt()
+    }
+
+}
---
+++ tasks/improve if else switch.js
@@ -0,0 +1,126 @@
+// const mark = Math.floor(Math.random() * 5 + 1); // Don't pay attention; in this variable random integer between 1 and 5
+// const result =
+//     mark === 5
+//         ? 'Excellent'
+//         : mark === 4
+//             ? 'Good'
+//             : mark === 3
+//                 ? 'Ok'
+//                 : mark === 2
+//                     ? 'Bad. Please, try better next time'
+//                     : mark === 1
+//                         ? 'Very bad'
+//                         : 'Unknown mark';
+
+
+function ifN() {
+    const n = 10
+    const mark = Math.floor(Math.random() * (n + 1))
+    if (mark === 5) {
+        console.log('Excellent');
+
+    } else if (mark === 4) {
+        console.log('Good');
+
+    } else if (mark === 3) {
+        console.log(OK);
+
+    } else if (mark === 2) {
+        console.log('Bad. Please, try better next time');
+
+    } else if (mark === 1) {
+        console.log('Very bad');
+
+    } else {
+        console.log('Unknown mark');
+
+    }
+}
+
+
+
+function switchN() {
+    const n = 5
+    const mark = Math.floor(Math.random() * (n + 1))
+    switch (mark) {
+        case 5:
+            console.log('Excellent');
+            break;
+
+        case 4:
+            console.log('GOOD');
+            break;
+
+        case 3:
+            console.log('Ok');
+            break;
+
+        case 2:
+            console.log('Bad.Please, try better next time');
+            break;
+
+        case 1:
+            console.log('Very bad');
+            break;
+
+        default:
+            console.log('Unknown mark');
+            break;
+    }
+}
+
+// ### Traffic light
+
+//     ** Goal ** - write a function that performs state of traffic light.
+//It should return a string that tells driver what to do.The function accepts a`color` 
+//as a single param. `
+//color` will be always a string:
+
+// - if color is read, return `Stop`
+//     - if color is green, return `Go`
+//         - if color is yellow, return `Prepare to go`
+//             - if color isn't red, green or yellow return `Unknown color`.
+
+//                 ** Important **: write 2 variants of check in function 
+//- with `switch` and with `if...else`.
+
+function trafficLight() {
+    const light = 3;
+    const read = 'Stop';
+    const green = 'Go';
+    const yellow = 'Prepare to go';
+
+    if (light === 1) {
+        return read
+
+    } else if (light === 2) {
+        return green
+
+    } else if (light === 3) {
+        return yellow
+
+    } else {
+
+    }
+}
+// console.log(trafficLight());
+
+function traffic() {
+    const light = 3;
+    const read = 'Stop';
+    const green = 'Go';
+    const yellow = 'Prepare to go';
+    switch (light) {
+        case 1:
+            return read
+            break;
+        case 2:
+            return green
+        case 3:
+            return yellow
+
+        default:
+            break;
+    }
+}
+// console.log(traffic());
