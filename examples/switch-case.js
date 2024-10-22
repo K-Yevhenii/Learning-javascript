45b5895123ebeb23677a9bde863ebf09eae2a411
Ihor Sokolov <i.sokolov@geniusee.com>
2024-10-22 09:50:41 +0300

    another round of renames

---
+++ examples/switch-case.js
@@ -0,0 +1,37 @@
+let season = prompt('Enter the season');
+
+function seasonAnswer() {
+  switch (season) {
+    case 'spring':
+      alert('spring');
+      break;
+    case 'summer':
+      alert('summer');
+      break;
+    case 'autumn':
+      alert('autumn');
+      break;
+    case 'summer':
+      alert('summer');
+      break;
+    default:
+      alert('Unknown season');
+  }
+}
+
+let mari = prompt('Маша');
+let dmitri = prompt('Діма');
+let anton = prompt('Антон');
+let lana = prompt('Светлана');
+let noData = prompt('No data available');
+let errorData = 'Error data available';
+
+// function rooClass7a() {
+//   switch (mari) {
+//     case
+//       break;
+
+//     default:
+//       break;
+//   }
+// }
