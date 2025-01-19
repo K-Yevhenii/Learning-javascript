e0a5a029e84ff55260a54f245c648f7248a0cf5f
Ihor Sokolov <i.sokolov@geniusee.com>
2025-01-19 15:36:55 +0200

    tasks

---
+++ daily-tasks/20.01/script.js
@@ -0,0 +1,25 @@
+// TODO: retrieve c and d
+const a = { b: { c: { d: 1 } } };
+
+// TODO:
+// 1. Copy array and add 10 to copied one
+// 2. Exclude from copy the first 2 elements
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// TODO: Merge two objects into one
+const first = { a: 1 };
+const second = { b: 2 };
+
+// TODO: exclude password from user
+const user = { firstName: 'John', lastName: 'Doe', email: 'john@doe.com', password: 'Qwerty123!' };
+
+// TODO: add new order to customer
+const customer = {
+  firstName: 'John',
+  lastName: 'Doe',
+  orders: [
+    { id: 1, sum: 2300 },
+    { id: 2, sum: 1500 },
+    { id: 3, sum: 3000 },
+  ],
+};
