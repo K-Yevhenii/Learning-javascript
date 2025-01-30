316db7c791c8d2c25cc8689368d8c0234a6d89a2
K-Yevhenii <ya.korniakov@gmail.com>
2025-01-30 11:26:28 +0200

    daily-tasks-27.01

--- daily-tasks/27.01/script.js
+++ daily-tasks/27.01/script.js
@@ -1,6 +1,6 @@
 const languages = {
   family: {
-    'Indo-European': {
+    IndoEuropean: {
       Germanic: ['English', 'German'],
       Roman: {
         WestRoman: ['Italian', 'Spanish', 'French'],
@@ -10,7 +10,12 @@ const languages = {
     },
   },
 };
-
+const languagesGerman = languages.family['Indo-European'].Germanic[1];
+const languagesFrench = languages.family['Indo-European'].Roman.WestRoman[2];
+const languagesGreek = languages.family['Indo-European'].Greek;
+const { family: { IndoEuropean: { Germanic: [, german] } } } = languages;
+const { family: { IndoEuropean: { Roman: { WestRoman: [, , french] } } } } = languages;
+const { family: { IndoEuropean: { Greek } } } = languages;
 // Retrieve German, French and Greek languages 1. using destructuring 2. and using dot notation
 
 const user = {
@@ -28,9 +33,13 @@ const user = {
 // 2.Get price of the last order 1. using destructuring 2. and using dot notation
 
 const keys = ['firstName', 'lastName', 'age'];
+user[keys[1]];
+const userOrder = user.orders[1].name;
+const lastOrder = user.orders[2].price;
+
+const { orders: [, { name, price: burgerPrice }, { price }] } = user
 
 // What will be printed in the console?
-user[keys[1]];
 
 const vehicles = [
   { type: 'car', brand: 'Toyota', model: 'Corolla', color: 'red' },
@@ -38,6 +47,8 @@ const vehicles = [
   { type: 'car', brand: 'Hyndai', model: 'Trail', color: 'orange' },
   { type: 'car', brand: 'Mazda', model: 'Corolla', color: 'red' },
 ];
-
+const typeCar = vehicles[2].type;
+const brandMoto = vehicles[1].brand;
+const [, { brand }, { type }] = vehicles;
 // 1. Get type of the orange vehicle 1. using destructuring 2. and using dot notation
 // 2. Get model of the motorbike 1. using destructuring 2. and using dot notation
--- tasks/answers/cart.js
+++ tasks/answers/cart.js
@@ -1,35 +1,84 @@
 const products = [
-    { id: 1, name: 'Banana', price: 20 },
-    { id: 2, name: 'Sausages', price: 40 },
-    { id: 3, name: 'Milk', price: 35 },
-    { id: 4, name: 'Mineral water', price: 12 },
+    { id: 1, name: "Banana", price: 20 },
+    { id: 2, name: "Sausages", price: 40 },
+    { id: 3, name: "Milk", price: 35 },
+    { id: 4, name: "Mineral water", price: 12 },
 ];
 
-let productsCart = {};
+// {
+//     let productsCart = {};
+//     function addProduct(productID) {
+//         const product = getProductByID(productID);
+//         if (productsCart[product.id]) {
+//             productsCart = {
+//                 ...productsCart, [product.id]: {
+//                     ...productsCart[product.id],
+//                     quantity: productsCart[product.id].quantity + 1
+//                 }
+//             }
+//         } else {
+//             productsCart = { ...productsCart, [product.id]: { ...product, quantity: 1 } }
+//         }
+//         return productsCart;
+//     }
+//     function getProductByID(productID) {
+//         for (const product of products) {
+//             const { id } = product;
+//             if (productID === product.id) {
+//                 //product.id
+//                 return product;
+//             }
+//         }
+//         return null;
+//     }
 
-function addProduct(productID) {
-    const product = getProductByID(productID);
+//     function removeProductFromCart(productID) {
+//         const product = getProductByID(productID);
+//         if (productsCart[productID]) {
+//             const { quantity } = productsCart[productID];
+//             if (quantity === 1) {
+//                 delete productsCart[productID];
+//             } else {
+//                 productsCart = {
+//                     ...productsCart, [productID]: {
+//                         ...productsCart[productID],
+//                         quantity: productsCart[productID].quantity - 1
+//                     }
+//                 }
+//             }
+//         }
+//         return productsCart;
+//     }
 
-    if (productsCart[product.id]) {
-        productsCart = {
-            ...productsCart,
-            [product.id]: {
-                ...productsCart[product.id],
-                quantity: productsCart[product.id].quantity + 1,
-            },
-        };
-    } else {
-        productsCart = { ...productsCart, [product.id]: { ...product, quantity: 1 } };
+//     console.log(addProduct(3));
+//     console.log(addProduct(4));
+
+// }
+
+
+
+let productsCart = [
+    { id: 1, name: 'Banana', price: 20, quantity: 1 },
+    { id: 2, name: 'Sausages', price: 40, quantity: 3 },
+    { id: 3, name: 'Milk', price: 35, quantity: 5 },
+    { id: 4, name: 'Mineral water', price: 12, quantity: 7 },
+];
+function getProductInCart(productID) {
+    for (let i = 0; i < productsCart.length; i++) {
+        const product = productsCart[i];
+        const { id } = product;
+        if (productID === id) {
+            //product.id
+            return i;
+        }
     }
-
-    return productsCart;
+    return null;
 }
 
 function getProductByID(productID) {
     for (const product of products) {
         const { id } = product;
-
-        if (productID === id) {
+        if (productID === product.id) {
             //product.id
             return product;
         }
@@ -37,52 +86,17 @@ function getProductByID(productID) {
     return null;
 }
 
-{
-    let productsCart = {};
-    function addProduct(productID) {
-        const product = getProductByID(productID);
-        if (productsCart[product.id]) {
-            productsCart = {
-                ...productsCart, [product.id]: {
-                    ...productsCart[product.id],
-                    quantity: productsCart[product.id].quantity + 1
-                }
-            }
-        } else {
-            productsCart = { ...productsCart, [product.id]: { ...product, quantity: 1 } }
-        }
-        return productsCart;
+function addProduct(productID) {
+    const productIndex = getProductInCart(productID);
+
+    if (productIndex !== null) {
+        productsCart = [...productsCart.slice(0, productIndex),
+        { ...productsCart[productIndex], quantity: productsCart[productIndex].quantity + 1 },
+        ...productsCart.slice(productIndex + 1)]
+    } else {
+        const productPromCatalog = getProductByID(productID);
+        productsCart = [...productsCart, { ...productPromCatalog, quantity: 1 }]
     }
-    function getProductByID(productID) {
-        for (const product of products) {
-            const { id } = product;
-            if (productID === product.id) {
-                //product.id
-                return product;
-            }
-        }
-        return null;
-    }
+}
 
-    function removeProductFromCart(productID) {
-        const product = getProductByID(productID);
-        if (productsCart[productID]) {
-            const { quantity } = productsCart[productID];
-            if (quantity === 1) {
-                delete productsCart[productID];
-            } else {
-                productsCart = {
-                    ...productsCart, [productID]: {
-                        ...productsCart[productID],
-                        quantity: productsCart[productID].quantity - 1
-                    }
-                }
-            }
-        }
-        return productsCart;
-    }
-
-    console.log(addProduct(3));
-    console.log(addProduct(3));
-
-
+addProduct(3);
