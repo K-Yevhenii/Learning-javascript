0562a0255d09420d962482c443288575249bb015
Ihor Sokolov <i.sokolov@geniusee.com>
2025-03-04 23:00:12 +0200

    add more rules and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,10 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
     rules: {
         indent: ['error', 4],
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        'max-len': ['error', { code: 100 }],
+        'comma-dangle': ['error', 'always-multiline'],
     },
     languageOptions: {
         globals: {
