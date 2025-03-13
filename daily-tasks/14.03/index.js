d2d9618e7f54828cbae2eb8f4c9841a39f46f27f
Ihor Sokolov <sihorjs@gmail.com>
2025-03-13 11:58:15 +0200

    push changes

---
+++ daily-tasks/14.03/index.js
@@ -0,0 +1,13 @@
+// TODO:
+// Find average complexity of all topics
+// Find the most complex topic and print it's name
+// Sort topics by name
+export const topics = [
+    { id: 1, name: 'JavaScript', complexity: 2 },
+    { id: 2, name: 'React', complexity: 3 },
+    { id: 3, name: 'Node', complexity: 4 },
+    { id: 4, name: 'MongoDB', complexity: 1 },
+    { id: 5, name: 'Express', complexity: 3 },
+    { id: 6, name: 'Redux', complexity: 3 },
+    { id: 7, name: 'TypeScript', complexity: 1 },
+];
