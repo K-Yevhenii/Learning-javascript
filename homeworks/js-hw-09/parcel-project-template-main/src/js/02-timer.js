1bc801997c77a20e2510444ba5bb9a7a95ade750
K-Yevhenii <ya.korniakov@gmail.com>
2025-04-29 12:03:52 +0300

    color-switcher

---
+++ homeworks/js-hw-09/parcel-project-template-main/src/js/02-timer.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
