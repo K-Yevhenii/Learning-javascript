67b6d33fbb01ce283799201aff748df9ec738e45
K-Yevhenii <ya.korniakov@gmail.com>
2025-05-02 21:05:47 +0300

    remove

--- examples/JS/cart/const getNumber = (value: number): numbe.ts
+++
@@ -1,4 +0,0 @@
-const getNumber = (value: number): number | undefined =>
-  number.find((element) => element === value);
-
-const getProperty = (key: string, obj: object): unknown | undefined => obj[key];
