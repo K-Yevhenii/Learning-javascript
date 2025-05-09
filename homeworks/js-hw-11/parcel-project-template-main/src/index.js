6da293e07a6523b046312e80c304b8b1016600d0
kornyakoff <dr.kornyakoff@gmail.com>
2025-05-09 13:07:29 +0300

    hw-11

--- homeworks/js-hw-11/parcel-project-template-main/src/index.js
+++ homeworks/js-hw-11/parcel-project-template-main/src/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import lightGallery from 'lightgallery';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const API_KEY = '50147129-6157c9390204a79bca0017f28';
@@ -10,16 +11,8 @@ const button = document.querySelector('.load-more');
 
 let page = 1;
 let searchQuery = '';
-// Step 2. Create a variable that will store total amount images
-
-/*
- * Step 5. Add function that will check can load more images
- * 1. Compare per_page * (page + 1) is bigger that total amount of images
- * 2. If yes, load more images
- * 3. If no, show error message "We're sorry, but you've reached the end of search results."
- *
- * Step 6. call this function on click of button 'Load more'
- */
+let totalImages;
+const IMAGES_PER_PAGE = 10;
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -30,9 +23,14 @@ form.addEventListener('submit', async (event) => {
 });
 
 button.addEventListener('click', async () => {
-    page += 1;
-    const images = await searchImages(searchQuery, page);
-    renderGallery(images);
+    if (checkHasNextPage()) {
+        page += 1;
+        const images = await searchImages(searchQuery, page);
+        renderGallery(images);
+
+    } else {
+        Notify.failure('We\'re sorry, but you\'ve reached the end of search results.');
+    }
 });
 
 async function searchImages(q, page) {
@@ -42,18 +40,19 @@ async function searchImages(q, page) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        // Step 4. Move to const under IMAGE_API_URL
-        per_page: 40,
+        per_page: IMAGES_PER_PAGE,
         page,
     };
 
     const response = await axios.get(IMAGE_API_URL, {
         params,
     });
-    console.log(response);
 
-    // Step 1. Return object with hits and totalHits
+    totalImages = response.data.totalHits;
+
     return response.data.hits;
+
+
 }
 
 function renderGallery(images) {
@@ -63,9 +62,9 @@ function renderGallery(images) {
         return;
     }
     const cards = images.map((image) => {
-        return `<div class="photo-card">
+        return `<a href=" <div class="photo-card" >
   <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-  <div class="info">
+  <div  class="info">
     <p class="info-item">
       <b>${image.likes} Likes</b>
     </p>
@@ -79,15 +78,22 @@ function renderGallery(images) {
       <b>${image.downloads} Downloads</b>
     </p>
   </div>
-</div>`;
+</div></a>`;
+
     }).join();
     gallery.innerHTML += cards;
     button.classList.remove('hidden');
+
 };
 
 function clearGallery() {
     gallery.innerHTML = '';
     page = 1;
-    // Step 3. Reset variable with total amount of images
+    button.classList.add('hidden');
+}
+
+function checkHasNextPage() {
+    return totalImages > (page + 1) * IMAGES_PER_PAGE;
+
 }
 
